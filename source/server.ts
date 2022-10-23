b2a698c6b9e1e4b7c0a6c608c5253e21dbfd47e1
Nari Park <npark11@myseneca.ca>
2022-10-23 16:12:07 -0400

    created the server

---
+++ source/server.ts
@@ -0,0 +1,54 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import logging from './config/logging';
+import config from './config/config';
+
+const NAMESPACE = 'Server';
+const router = express();
+
+/** Logging the request */
+router.use((req, res, next) => {
+    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${req.statusCode}]`);
+    });
+
+    next();
+});
+
+/** Parse the request */
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+/** Rules of our API */
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Typed, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Origin', 'GET PATCH DELETE POST PUT');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+/** Routes */
+
+/** Error Handling */
+router.use((req, res, next) => {
+    const error = new Error('not found');
+
+    return res.status(404).json({
+        message: error.message
+    });
+});
+
+/** Create the Server */
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.port}`));
+
+/**  */
+/**  */
