fdd98555020cf6a2892d12d1c9576aa5f5ebacc4
Nari Park <npark11@myseneca.ca>
2022-10-26 15:30:08 -0400

    Connected Mongo & Created Model/Interface/Functions/Routes

--- source/config/config.ts
+++ source/config/config.ts
@@ -2,6 +2,26 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const MONGO_OPTIONS = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    socketTimeoutMS: 30000,
+    keepAlive: true,
+    autoIndex: false,
+    retryWrites: false
+};
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || 'oliveOil';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'oliveOilPasta89';
+
+const MONGO = {
+    //    host: MONGO_HOST,
+    username: MONGO_USERNAME,
+    password: MONGO_PASSWORD,
+    options: MONGO_OPTIONS,
+    url: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.ujhrstl.mongodb.net/?retryWrites=true&w=majority`
+};
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
@@ -11,6 +31,7 @@ const SERVER = {
 };
 
 const config = {
+    mongo: MONGO,
     server: SERVER
 };
 
